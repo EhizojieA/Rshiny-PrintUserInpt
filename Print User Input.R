9a290c906b612323a148d86d861fab05452683bd
EhizojieA <33437931+EhizojieA@users.noreply.github.com>
2023-03-29 15:25:14 -0400

    Update Print User Input.R
    Moved widgets around for presentation.

--- Print User Input.R
+++ Print User Input.R
@@ -3,25 +3,27 @@ library(shinythemes)
 
 
 
-ui <- fluidPage(theme = shinytheme("superhero"),
-  navbarPage(
-  
-  tabPanel("Navbar 1",
-           sidebarPanel(
-             tags$h3("Input 1"),
-             textInput("txt 1", "anything", ""),
-             textInput("txt 2", "anything", ""),
-           ),
-  mainPanel(
-    h1("Header 1"),
-    h4("Header 4"),
-    verbatimTextOutput("txtout"),
-  )
-  ),
-
-tabPanel("Navigation 1", "Blank"),
-tabPanel("Navigation 2", "Blank")
-)
+ui <- fluidPage(theme = shinytheme("cerulean"),
+                navbarPage(
+                  
+                  tabPanel("Navbar 1",
+                           sidebarPanel(
+                             tags$h3("Input 1"),
+                             textInput("txt 1", "anything", ""),
+                             textInput("txt 2", "anything", ""),
+                           ),
+                           mainPanel(
+                             h1("Header 1"),
+                             h4("Header 4"),
+                             verbatimTextOutput("txtout"),
+                           )
+                  ),
+                  
+                  tabPanel("Navigation 1", "Blank"),
+                  tabPanel("Navigation 2", "Blank"),
+                  
+                ),
+             
 )
 
 server = function(input, output){
@@ -33,4 +35,3 @@ server = function(input, output){
 }
 
 shinyApp(ui = ui, server = server)
-  
